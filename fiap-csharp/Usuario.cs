f409aa4a03db7e68522a3a99a3bd5371be2c27e7
Lucas Simidu <ldsimidu@gmail.com>
2026-03-12 17:28:28 -0300

    Create Usuario.cs

---
+++ fiap-csharp/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace fiap_csharp
+{
+    public class Usuario
+    {
+        [JsonPropertyName("nome")]
+        public string Nome { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("senha")]
+        public string Senha { get; set; }
+    }
+}
